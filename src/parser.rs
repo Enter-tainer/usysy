805281c8d6883f1b41b0b9d3c1634efa0ad4459a
mgt <mgt@oi-wiki.org>
2022-06-16 00:08:33 +0800

    add pretty print tree

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,6 @@
 use crate::error::{Error, Result};
 use colored::*;
+use itertools::Itertools;
 use tree_sitter::{Node, Parser, Tree};
 pub fn parse(input: &str) -> Result<Tree> {
   let mut parser = Parser::new();
@@ -11,29 +12,48 @@ pub fn parse(input: &str) -> Result<Tree> {
   Ok(tree)
 }
 #[allow(dead_code)]
-fn dump_node_internal(node: &Node, level: usize, content: &str) {
+fn dump_node_internal(node: &Node, prefix: &str, content: &str, is_last: bool, is_init: bool) {
   let node_text = node.utf8_text(content.as_bytes()).unwrap();
+  let start = node.start_position();
+  let end = node.end_position();
+  let kind = node.kind();
   println!(
-    "{}{:?}{}",
-    " ".repeat(level),
-    node,
+    "{}{}`{}` {} - {}{}",
+    prefix,
+    if is_init {
+      ""
+    } else if is_last {
+      "└──"
+    } else {
+      "├──"
+    },
+    kind.bold(),
+    start,
+    end,
     if node.child_count() == 0 || !node_text.contains('\n') {
-      format!(
-        " {} {}",
-        "->".cyan(),
-        node_text.bold()
-      )
-      .bold()
+      format!(" {} {}", "->".cyan(), node_text.bold()).bold()
     } else {
       "".to_owned().normal()
     }
   );
   let mut cursor = node.walk();
   if cursor.goto_first_child() {
+    let prefix = format!("{}{}   ", prefix, if is_last { " " } else { "│" });
+    let mut nodes = Vec::new();
     loop {
-      dump_node_internal(&cursor.node(), level + 2, content);
+      nodes.push(cursor.node());
       if !cursor.goto_next_sibling() {
         break;
+      }
+    }
+    for i in nodes.into_iter().with_position() {
+      match i {
+        itertools::Position::First(i) | itertools::Position::Middle(i) => {
+          dump_node_internal(&i, &prefix, content, false, false);
+        }
+        itertools::Position::Last(i) | itertools::Position::Only(i) => {
+          dump_node_internal(&i, &prefix, content, true, false);
+        }
       }
     }
   }
@@ -41,5 +61,5 @@ fn dump_node_internal(node: &Node, level: usize, content: &str) {
 
 #[allow(dead_code)]
 pub fn dump_node(node: &Node, content: &str) {
-  dump_node_internal(node, 0, content);
+  dump_node_internal(node, "", content, true, true);
 }
